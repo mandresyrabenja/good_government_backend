982406bb0bff2fd1f8570b2b1b77a973f546a227
Mandresy Rabenja <mandresyrabenj@gmail.com>
2022-01-13 11:32:17 +0300

    feat!: Change authentication from base auth to form auth

---
+++ src/main/java/mg/gov/goodGovernment/controller/Controller.java
@@ -0,0 +1,18 @@
+package mg.gov.goodGovernment.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.stereotype.Controller
+@RequestMapping("/")
+public class Controller {
+
+    /**
+     * Formulaire du login
+     */
+    @GetMapping("login")
+    public String getLogin() {
+        return "login";
+    }
+
+}
--- src/main/java/mg/gov/goodGovernment/security/SecurityConfig.java
+++ src/main/java/mg/gov/goodGovernment/security/SecurityConfig.java
@@ -12,7 +12,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Configuration du securité de l'application
@@ -27,12 +29,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable() // Cet application va être utilisé par des clients web et mobile
-                .authorizeRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
+            .csrf().disable() // Cet application va être utilisé par des clients web et mobile
+            .authorizeRequests()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .formLogin()
+            .loginPage("/login").permitAll()
+            .and()
+            .rememberMe().tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(24))
+            .and()
+            .logout()
+                .logoutUrl("/logout")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                .clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID", "remember-me")
+                .logoutSuccessUrl("/login");
     }
 
     @Override
